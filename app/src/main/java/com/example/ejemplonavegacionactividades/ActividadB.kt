34b7dd76b3605880eb90c123d7fa101b7c673fd4
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-03-06 15:50:45 -0500

    Errores de navegación

--- app/src/main/java/com/example/ejemplonavegacionactividades/ActividadB.kt
+++ app/src/main/java/com/example/ejemplonavegacionactividades/ActividadB.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class ActividadB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +14,19 @@ class ActividadB : AppCompatActivity() {
         val botonc = findViewById<Button>(R.id.botonc)
         val botond = findViewById<Button>(R.id.botond)
 
+        val mensaje = intent.getStringExtra("MENSAJE")
+
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+
         botonc.setOnClickListener{
             var intent = Intent(this, ActividadC::class.java)
+            intent.putExtra("MENSAJE", "Hacia Actividad C")
+            intent.putExtra("MENSAJEA", mensaje)
             startActivity(intent)
         }
         botond.setOnClickListener{
             var intent = Intent(this, ActividadD::class.java)
+            intent.putExtra("MENSAJE", "Hacia Actividad D")
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/ejemplonavegacionactividades/ActividadC.kt
+++ app/src/main/java/com/example/ejemplonavegacionactividades/ActividadC.kt
@@ -1,11 +1,27 @@
 package com.example.ejemplonavegacionactividades
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class ActividadC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad_c)
+
+        val mensaje = intent.getStringExtra("MENSAJE")
+        val mensajeDeA = intent.getStringExtra("MENSAJEA")
+        val btnC = findViewById<Button>(R.id.btnC)
+
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+
+        btnC.setOnClickListener{
+            val intent = Intent(this, ActividadB::class.java)
+            intent.putExtra("MENSAJE", mensajeDeA)
+            startActivity(intent)
+        }
+
     }
 }
